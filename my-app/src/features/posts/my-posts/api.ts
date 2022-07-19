4cf3dd2fe439cb5d4cff4bd54ea1c1f06ceb3822
KarinaKushnir <karina.kushnir97@gmail.com>
2022-07-19 13:52:49 +0300

    change my posts api, add token

--- my-app/src/features/posts/my-posts/api.ts
+++ my-app/src/features/posts/my-posts/api.ts
@@ -1,9 +1,13 @@
 import { baseUrl } from "../../../api/config";
 
 export namespace MyPostsApi {
-  export async function getMyPostsFetch() {
+  export async function getMyPostsFetch(accessToken: string) {
     try {
-      const result = await fetch(`${baseUrl}blog/posts/my_posts/`);
+      const result = await fetch(`${baseUrl}blog/posts/my_posts/`, {
+        method: "GET",
+        headers: { authorization: `Bearer ${accessToken}` },
+      });
+
       if (!result.ok) {
         const errorText = await result.text();
         throw new Error(errorText);
